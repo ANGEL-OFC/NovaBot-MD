1c73d215415137952619fd3d641765e44db5a58b
ANGEL-OFC <149626423+ANGEL-OFC@users.noreply.github.com>
2025-09-11 17:33:49 -0500

    Actualizar update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 # Código desarrollado por @gata_dios 
 
 #!/data/data/com.termux/files/usr/bin/bash 
-BOT_DIR="SakuraBot-MD"  
-BOT_REPO="https://github.com/DevAlexJs/$BOT_DIR"
+BOT_DIR="NovaBot-MD"  
+BOT_REPO="https://github.com/ANGEL-OFC/$BOT_DIR"
 DB_FILE="datos.json"
 
 GREEN='\033[32m'
