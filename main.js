895c22b47de8eb9755be728d0195397d81ffe7d2
ANGEL-OFC <149626423+ANGEL-OFC@users.noreply.github.com>
2025-10-02 16:12:49 -0500

    Actualizar main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-require('events').EventEmitter.defaultMaxListeners = 20;
+require('events').EventEmitter.defaultMaxListeners = 50;
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './config.js'
 import './plugins/_content.js'
