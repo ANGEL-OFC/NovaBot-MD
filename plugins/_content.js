5b66704a5af942e7f665468eba858d9d90e41c5b
ANGEL-OFC <149626423+ANGEL-OFC@users.noreply.github.com>
2025-10-11 08:35:02 -0500

    Actualizar _content.js

--- plugins/_content.js
+++ plugins/_content.js
@@ -188,7 +188,7 @@ global.img18 = img //'https://qu.ax/HCt.jpg'
 
 global.logogit = img //'https://qu.ax/RzKF.jpg'
 
-global.gataVidMenu = ['https://qu.ax/XYWjE.jpg', 'https://qu.ax/XYWjE.jpg'][Math.floor(Math.random() * 2)]
+global.novaVidMenu = ['https://qu.ax/XYWjE.jpg', 'https://qu.ax/XYWjE.jpg'][Math.floor(Math.random() * 2)]
 global.gataMenu = [img, img2, img6, img7, img8, img9, img13, img14, img15, img17, img18][Math.floor(Math.random() * 11)]
 global.gataImg = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11, imagen12, imagen13][
 Math.floor(Math.random() * 13)
